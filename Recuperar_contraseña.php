77d6d8b26b3af3f7774c487e74cc86a9262a05d9
saraid-graterol <saraidgraterol06@gmail.com>
2023-10-24 23:52:55 -0400

    listo el css

--- Recuperar_contraseña.php
+++ Recuperar_contraseña.php
@@ -3,6 +3,10 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="./css/Recuperar_contraseña.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,100&display=swap" rel="stylesheet">
 	<title>¿Has olvidado la contraseña? | No puedo entrar | Escuela de Trasnporte</title>
 </head>
 <body>
@@ -15,10 +19,12 @@
 	        <input type="password" name="password" placeholder="contraseña"><br>
 	        <label>Confirmar contraseña</label><br>
 	        <input type="password" name="password2" placeholder="contraseña"><br>
-		</div>
-	<button type="submit" name="buscar">restablecer</button></a>
+				</div>
+				<div class="btn-actions">
+	<button type="submit" name="buscar">Guardar</button>
+	<a href="Iniciodeseccion.php"><button type= "button">Cancelar</button></a>
+				</div>
 	</form>
-	<a href="Iniciodeseccion.php"><button>Cancelar</button></a>
 </body>
 </html>
 <?php
