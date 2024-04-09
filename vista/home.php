9a80defcacda0a745393c560b3588576ec06fd05
saraid-graterol <saraidgraterol06@gmail.com>
2024-04-09 00:17:27 -0400

    css de juan y el home

--- vista/home.php
+++ vista/home.php
@@ -1,1 +1,68 @@
-hola
+<head>
+  <link rel="stylesheet" type="text/css" href="./css/home.css">
+  <script src="https://kit.fontawesome.com/eb496ab1a0.js" crossorigin="anonymous"></script>
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,100&display=swap" rel="stylesheet">
+</head>
+<body>
+  <div class="container">
+  <h1><img src="./Imagenes/titulo_de_proyectoasdfg-removebg-preview.jpeg" alt="Imagen 1" width="800px"></h1>
+
+    <div class="cuadro">
+      <h2></h2>
+      <p></p>
+      <img src="./Imagenes/Informacion 1.jpeg" alt="Imagen 1">
+    </div>
+    <div class="cuadro">
+      <h2></h2>
+      <p></p>
+      <img src="./Imagenes/Informacion 3.jpeg" alt="Imagen 2">
+    </div>
+        <div class="cuadro">
+      <h2></h2>
+      <p></p>
+      <img src="./Imagenes/Informacion 2.jpeg" alt="Imagen 1">
+    </div>
+    <div class="cuadro1">
+      <h3>
+      <p></p>
+      <img src="./Imagenes/sistema de gestion (2).jpeg" alt="Imagen 3">
+    </h3>
+    </div>
+  </div>
+
+<!--::::Pie de Pagina::::::-->
+  <footer class="pie-pagina">
+        <div class="grupo-1">
+            <div class="box"> 
+                <figure>
+                    <a href="#">
+                        <img src="" alt="">
+                    </a>
+                </figure>
+            </div>
+            <div class="box">
+                <h2>SOBRE NOSOTROS</h2>
+                <p>Este progrma fue creado y desarrollado por las siguientes personas:</p>
+                <p> Angel Perez</p>
+                <p>Saraid Graterol</p>
+                <p>Juan Calero</p>
+            </div>
+            <div class="box">
+                <h2>SIGUENOS</h2>
+                <div class="red-social">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                </div>
+            </div>
+        </div>
+        <div class="grupo-2">
+            <small>&copy; 2024 <b></b> - Todos los Derechos Reservados.</small>
+        </div>
+    </footer> 
+ 
+
+</body>
+</html>
